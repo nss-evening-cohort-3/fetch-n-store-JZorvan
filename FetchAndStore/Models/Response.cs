e92e26adfdd194ed5d628309c1a4996d664debf1
JZorvan <jzorvan@gmail.com>
2016-10-25 19:49:14 -0700

    Added Response Model

---
+++ FetchAndStore/Models/Response.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FetchAndStore.Models
+{
+    public class Response
+    {
+        [Key]
+        public int ResponseID { get; set; }
+
+        [Required]
+        public string MethodUsed { get; set; }
+
+        [Required]
+        public string UserURL { get; set; }
+
+        [Required]
+        public int StatusCode { get; set; }
+
+        [Required]
+        public string ResponseTime { get; set; }
+    }
+}
