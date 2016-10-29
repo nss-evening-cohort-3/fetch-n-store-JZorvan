8b5abfcdec98d1db187dcb00bc3b7218970acb61
JZorvan <jzorvan@gmail.com>
2016-10-29 11:36:26 -0700

    Fetch form working

--- FetchAndStore/App/App.js
+++ FetchAndStore/App/App.js
@@ -1,1 +1,21 @@
-﻿
+﻿var app = angular.module("FetchAndStore", []);
+
+app.controller("FetchCtrl", ['$scope', '$http', function ($scope, $http) {
+
+
+
+    $scope.goFetch = () => {
+        alert("Hello World");
+        $scope.UserMethod;
+        $scope.UserURL;
+        console.log($scope.UserURL);
+        $http.get($scope.UserURL);
+        //$http.post("/api/Response", { "Name": "Janelle", "Class": "E3" })
+        //    .success(function (response) {
+        //    })
+        //    .error(function (response) {
+        //        alert("Bad!");
+        //    });
+    }
+}]);
+
